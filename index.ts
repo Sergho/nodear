45327ddcfffbfefa5a0d1a6c3ce865b77841f490
Sergho <sergche04@gmail.com>
2024-10-25 22:45:04 +0300

    feat: mic listen while `alt` key pressed

--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
 import Microphone from 'node-microphone';
+import { GlobalKeyboardListener } from 'node-global-key-listener';
 import * as fs from 'fs';
+
+const globalListener = new GlobalKeyboardListener();
+
+globalListener.addListener((e) => {
+  if (e.vKey == 64) {
+    if (e.state === 'DOWN') prepare();
+    if (e.state === 'UP') finish();
+  }
+});
 
 const mic = new Microphone({
   device: 'default',
   rate: 44100,
 });
-const fileStream = fs.createWriteStream('audio/command.wav');
-const micStream = mic.startRecording();
 
-micStream.pipe(fileStream);
+function prepare() {
+  console.log('Recording started');
+  const micStream = mic.startRecording();
+  const fileStream = fs.createWriteStream('audio/command.wav');
+  micStream.pipe(fileStream);
+}
 
-setTimeout(() => {
-  console.log('Recording finished!');
+function finish() {
   mic.stopRecording();
-}, 3000);
+  console.log('Recording finished!');
+}
